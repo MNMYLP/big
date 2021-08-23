92ab653ab8c6f1ae0b8cff756ac3ac3d595e11ba
mose <huijiu028@163.com>
2021-08-23 20:06:25 +0800

    代码

---
+++ assets/js/login.js
@@ -0,0 +1,16 @@
+$(function(){
+    //点击注册账号
+    $('#link_reg').on('click',function(){
+        $('.login-box').hide()
+        $('.reg-box').show()
+    })
+
+    $('#link_login').on('click',function(){
+        $('.login-box').show()
+        $('.reg-box').hide()
+    })
+
+
+
+
+})
